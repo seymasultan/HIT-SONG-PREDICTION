8c5ebf936b07b1d1da224d0ac56f0b722a35b17d
seymasultan <66266079+seymasultan@users.noreply.github.com>
2020-11-09 23:40:26 +0300

    SVM and NaiveBayes is added.

--- SVM.py
+++ SVM.py
@@ -1,7 +1,8 @@
 from sklearn.model_selection import train_test_split
 import numpy as np
-from sklearn.metrics import confusion_matrix
-from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.svm import SVC
+
 import SpotifyConnection
 import dataset
 
@@ -24,20 +25,23 @@ def listNormalizer(mylist: np.ndarray):
 def model(allSong, targetList):
     predicted=[]
     X_train, X_test, y_train, y_test = train_test_split(allSong, targetList, test_size=0.2)
-    bnb=GaussianNB()
-    bnb.fit(X_train, y_train)
+    # Parametre olarak farklı kernel trick tipleri verilebilir.
+    # Başarı oranının değiştiği gözlemlenecektir. ( ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’ )
+    # kernelin default değeri 'rbf' dir.
+    svc = SVC()
+    svc.fit(X_train, y_train)
 
     for i in range(len(X_test)):
         predict_me = np.array(X_test[i].astype(float))
         predict_me = predict_me.reshape(-1, len(predict_me))
-        prediction = bnb.predict(predict_me)
+        prediction = svc.predict(predict_me)
         predicted.append(prediction)
 
     print(confusion_matrix(y_test, predicted))
-    print("Accuracy of Decision Tree classifier on training set: {:.2f}".format(bnb.score(X_train, y_train)))
-    print("Accuracy of Decision Tree classifier on test set: {:.2f}".format(bnb.score(X_test, y_test)))
+    print("Accuracy of Decision Tree classifier on training set: {:.2f}".format(svc.score(X_train, y_train)))
+    print("Accuracy of Decision Tree classifier on test set: {:.2f}".format(svc.score(X_test, y_test)))
 
-    songUri = "spotify:track:1aaI0imelqLqye35922oMD"
+    songUri = "spotify:track:4WQWrSXYLnwwcmdNk8dYqN"
     if songUri.find("spotify") != -1:
         songUri = songUri[14:]
 
@@ -48,7 +52,7 @@ def model(allSong, targetList):
     allSong = allSong / allSong.max(axis=0)
 
     mysong = allSong[-1:]
-    y_pred = bnb.predict(mysong)
+    y_pred = svc.predict(mysong)
     print(y_pred)
     print("Sanatçı:" + artistName)
     print("Şarkı Adı:" + songName)
